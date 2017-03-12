59aaeb4a1f72cd388e6dbe903af2907fde97f05a
nanoshematic <nanoshematic@gmail.com>
2017-03-12 19:03:11 +0300

    translation

---
+++ QmlLanguage_ru.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>OptionsTab</name>
+    <message>
+        <source>Основные опции</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Расширенные опции</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Дополнительные опции</source>
+        <translation type="unfinished">additional options</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Запустить</source>
+        <translation type="unfinished">Запустить русский</translation>
+    </message>
+</context>
+</TS>
---
+++ translatontest.h
@@ -0,0 +1,11 @@
+#ifndef TRANSLATONTEST_H
+#define TRANSLATONTEST_H
+
+
+class TranslatonTest
+{
+public:
+    TranslatonTest();
+};
+
+#endif // TRANSLATONTEST_H
